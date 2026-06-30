d83514659f54a3ea5897b77a028bc29f0b4a7a14
Coolbit <neeoub@gmail.com>
2026-06-30 08:37:02 +0545

    feat: added TestDecode test to test decoding function from internal/encoding

--- internal/encoding/encoding_test.go
+++ internal/encoding/encoding_test.go
@@ -15,7 +15,7 @@ func TestEncode(t *testing.T) {
 		{"Encode byte array", []byte("yamraj"), false},
 		{"Encode empty byte array", []byte(""), true},
 		{"Encode single byte array", []byte("a"), false},
-		{"Encode bytes with all possible values [0-255]", []byte{0, 27, 127, 
+		{"Encode bytes with all possible values [0-255]", []byte{0, 27, 127,
 			255, 128, 64}, false},
 	}
 	for _, tc := range cases {
@@ -35,5 +35,28 @@ func TestEncode(t *testing.T) {
 }
 
 func TestDecode(t *testing.T) {
+	validEncoded, err := Encode([]byte("yamraj"))
+	require.NoError(t, err)
+	cases := []struct {
+		name    string
+		encoded string
+		wantErr bool
+	}{
+		{"Decode valid encoded data", validEncoded, false},
+		{"Decode invalid data", "not!!valid=data", true},
+		{"Decode data with padding", "eWFtcmFq==", true},
+		{"Decode empty data", "", true},
+	}
 
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			decoded, err := Decode(tc.encoded)
+			if !tc.wantErr {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Empty(t, decoded)
+			}
+		})
+	}
 }
